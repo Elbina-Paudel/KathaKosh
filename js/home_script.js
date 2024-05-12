960cfe4a337c7194cf670fbbcb2e724e7242cb35
Punam Dahal <dahalpunam68@gmail.com>
2024-05-12 09:21:59 +0545

    updated css

--- js/home_script.js
+++ js/home_script.js
@@ -9,14 +9,19 @@ function goToKoshPage() {
 document.getElementById('kathaButton').addEventListener('click', goToKathaPage);
 document.getElementById('koshButton').addEventListener('click', goToKoshPage);
 
+document.addEventListener('DOMContentLoaded', function() {
+    const slider = document.getElementById('imageSlider');
+    const imagesFolder = '../images/'; 
 
+    const imageFilenames = ['janakpur.jpg', 'kumari.jpg', '1.jpg', '2.jpg', '3.jpg', '4.jpg','5.jpg', '6.jpg', '7.jpg', '8.jpg'];
 
-
-
-
-
-
-
+    imageFilenames.forEach(function(filename) {
+        const img = document.createElement('img');
+        img.src = imagesFolder + filename;
+        img.alt = filename; 
+        slider.appendChild(img); 
+    });
+});
 
     // Function to check scroll direction
     let lastScrollTop = 0;
